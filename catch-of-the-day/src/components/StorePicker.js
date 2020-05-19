7176165b882c8a011d5342f6b444f1717614d4d5
Brad <bradleofabian@gmail.com>
2020-05-19 12:17:25 -0400

    set up menu with header and inventory

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello!</p>;
+    return (
+      <Fragment>
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+          <input type="text" required placeholder="Enter Store Name" />
+          <button type="submit">Visit Store</button>
+        </form>
+      </Fragment>
+    );
   }
 }
 
