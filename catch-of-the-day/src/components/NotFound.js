561ffa2425c42d6c1e33b1eefcbc6d611d670fd6
Brad <bradleofabian@gmail.com>
2020-05-19 14:33:03 -0400

    made not found and router component

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+{
+  /*Statless arrow function  */
+}
+const NotFound = () => (
+  <div>
+    <header>
+      <h2> 401 Not Found</h2>
+    </header>
+  </div>
+);
+
+export default NotFound;
