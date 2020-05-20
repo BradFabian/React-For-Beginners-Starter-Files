f8be2840f469e82d8be4257aa774e4d500c9d484
Brad <bradleofabian@gmail.com>
2020-05-19 22:34:52 -0400

    added push from log in to main page

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,26 @@ import React, { Fragment } from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  //To access THIS in custom method declare a property set to arrow function//
+  goToStore = (event) => {
+    //1. Stop form from submitting
+    event.preventDefault();
+
+    //2. get text from that input
+    const storeName = this.myInput.current.value;
+
+    //3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
       <Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Enter Store Name"
             defaultValue={getFunName()}
